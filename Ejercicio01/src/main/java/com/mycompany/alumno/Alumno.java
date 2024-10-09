a8508ad2d263afb9be1f4a743af29603d9713f8d
HilterNes <hilter.626@gmail.com>
2024-10-09 00:34:21 -0500

    Add files via upload

---
+++ Ejercicio01/src/main/java/com/mycompany/alumno/Alumno.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.alumno;
+
+import vista.Registro;
+
+public class Alumno {
+
+    public static void main(String[] args) {
+        
+        Registro dato = new Registro();
+        dato.show();
+
+    }
+    
+}
